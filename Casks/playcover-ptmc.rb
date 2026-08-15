75655840ca50c2ac98f23b2b547ce078cb1dc5f2
EmilyTsum <297192186+EmilyTsum@users.noreply.github.com>
2026-08-15 09:51:02 +0000

    chore: bump playcover-ptmc to 0.1.7

--- Casks/playcover-ptmc.rb
+++ Casks/playcover-ptmc.rb
@@ -1,6 +1,6 @@
 cask "playcover-ptmc" do
-  version "0.1.5"
-  sha256 "1ef84be9bba16a708c32489dba9d8d38c9c45b25978e24eaca004ec0c0280375"
+  version "0.1.7"
+  sha256 "51bedf0620973cce84a0e9e4de38f00ad0f542122047173aa9d604d0e4d15bfb"
 
   url "https://github.com/EmilyTsum/PlayCover/releases/download/ptmc-v#{version}/PlayCover-PTMC.dmg"
   name "PlayCover PTMC"
